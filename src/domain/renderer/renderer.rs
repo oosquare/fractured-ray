3d4c303e10675e39688d08b825e82253190a00af
oosquare <oosquare@outlook.com>
2025-07-01 00:21:33 +0800

    feat(domain): add ray casting

--- src/domain/renderer/renderer.rs
+++ src/domain/renderer/renderer.rs
@@ -1,6 +1,11 @@
-use crate::domain::camera::Camera;
+use std::ops::Bound;
+
+use crate::domain::camera::{Camera, Offset};
 use crate::domain::color::Color;
+use crate::domain::entity::shape::{DisRange, Shape, Sphere};
+use crate::domain::geometry::Point;
 use crate::domain::image::Image;
+use crate::domain::ray::{Ray, RayTrace};
 
 #[derive(Debug)]
 pub struct Renderer {
@@ -16,14 +21,37 @@ impl Renderer {
         let mut image = Image::new(self.camera.resolution().clone());
         for row in 0..image.resolution().height() {
             for column in 0..image.resolution().width() {
-                let color = Color::new(
-                    row as f64 / image.resolution().height() as f64,
-                    column as f64 / image.resolution().width() as f64,
-                    0.0,
+                let point = self
+                    .camera
+                    .calc_point_in_pixel(row, column, Offset::center())
+                    .expect("row and column should not be out of bound");
+                let direction = (point - self.camera.position())
+                    .normalize()
+                    .expect("focal length should be positive");
+                let ray = self.trace(
+                    RayTrace::new(point, direction),
+                    (Bound::Excluded(0.0), Bound::Unbounded),
                 );
-                image.record(row, column, color);
+                image.record(row, column, ray.color());
             }
         }
         image
+    }
+
+    fn trace(&self, ray: RayTrace, range: DisRange) -> Ray {
+        let sphere = Sphere::new(Point::new(0.0, 0.0, -3.0), 1.0).unwrap();
+        if let Some(intersection) = sphere.hit(&ray, range) {
+            let normal = intersection.normal();
+            Ray::new(
+                RayTrace::new(ray.start(), -ray.direction()),
+                Color::new(
+                    normal.x() / 2.0 + 0.5,
+                    normal.y() / 2.0 + 0.5,
+                    normal.z() / 2.0 + 0.5,
+                ),
+            )
+        } else {
+            Ray::new(RayTrace::new(ray.start(), -ray.direction()), Color::BLACK)
+        }
     }
 }
