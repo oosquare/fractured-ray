2671db70d72e8de5d1b70568f27121e5e05566d9
oosquare <oosquare@outlook.com>
2025-07-07 00:43:04 +0800

    feat: add cornell box example

---
+++ examples/cornell_box.rs
@@ -0,0 +1,150 @@
+use std::error::Error;
+use std::fs::File;
+
+use smallvec::smallvec;
+
+use fractured_ray::domain::camera::{Camera, Resolution};
+use fractured_ray::domain::color::Color;
+use fractured_ray::domain::entity::Scene;
+use fractured_ray::domain::entity::material::{Diffuse, Emissive};
+use fractured_ray::domain::entity::shape::Polygon;
+use fractured_ray::domain::geometry::{Point, UnitVector, Val};
+use fractured_ray::domain::renderer::{Configuration, CoreRenderer, Renderer};
+use fractured_ray::infrastructure::image::PngWriter;
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let camera = Camera::new(
+        Point::new(Val(278.0), Val(273.0), Val(-800.0)),
+        UnitVector::z_direction(),
+        Resolution::new(800, (1, 1))?,
+        Val(0.025),
+        Val(0.035),
+    )?;
+
+    let mut scene = Scene::new();
+
+    // Light
+    scene.add(
+        Polygon::new([
+            Point::new(Val(343.0), Val(548.0), Val(227.0)),
+            Point::new(Val(343.0), Val(548.0), Val(332.0)),
+            Point::new(Val(213.0), Val(548.0), Val(332.0)),
+            Point::new(Val(213.0), Val(548.0), Val(227.0)),
+        ])?,
+        Emissive::new(Color::new(Val(0.9), Val(0.85), Val(0.4)) * Val(5.0)),
+    );
+
+    // Floor
+    scene.add(
+        Polygon::new([
+            Point::new(Val(552.8), Val(0.0), Val(0.0)),
+            Point::new(Val(0.0), Val(0.0), Val(0.0)),
+            Point::new(Val(0.0), Val(0.0), Val(559.2)),
+            Point::new(Val(549.6), Val(0.0), Val(559.2)),
+        ])?,
+        Diffuse::new(Color::WHITE),
+    );
+
+    // Ceiling
+    scene.add(
+        Polygon::new([
+            Point::new(Val(556.0), Val(548.8), Val(0.0)),
+            Point::new(Val(556.0), Val(548.8), Val(559.2)),
+            Point::new(Val(0.0), Val(548.8), Val(559.2)),
+            Point::new(Val(0.0), Val(548.8), Val(0.0)),
+        ])?,
+        Diffuse::new(Color::WHITE),
+    );
+
+    // Left Wall
+    scene.add(
+        Polygon::new([
+            Point::new(Val(549.6), Val(0.0), Val(0.0)),
+            Point::new(Val(549.6), Val(0.0), Val(559.2)),
+            Point::new(Val(556.0), Val(548.8), Val(559.2)),
+            Point::new(Val(556.0), Val(548.8), Val(0.0)),
+        ])?,
+        Diffuse::new(Color::RED),
+    );
+
+    // Right Wall
+    scene.add(
+        Polygon::new([
+            Point::new(Val(0.0), Val(0.0), Val(559.2)),
+            Point::new(Val(0.0), Val(0.0), Val(0.0)),
+            Point::new(Val(0.0), Val(548.8), Val(0.0)),
+            Point::new(Val(0.0), Val(548.8), Val(559.2)),
+        ])?,
+        Diffuse::new(Color::GREEN),
+    );
+
+    // Back Wall
+    scene.add(
+        Polygon::new([
+            Point::new(Val(549.6), Val(0.0), Val(559.2)),
+            Point::new(Val(0.0), Val(0.0), Val(559.2)),
+            Point::new(Val(0.0), Val(548.8), Val(559.2)),
+            Point::new(Val(556.0), Val(548.8), Val(559.2)),
+        ])?,
+        Diffuse::new(Color::WHITE),
+    );
+
+    // Short Block
+    scene.add_mesh(
+        smallvec![
+            Point::new(Val(130.0), Val(165.0), Val(65.0)),
+            Point::new(Val(82.0), Val(165.0), Val(225.0)),
+            Point::new(Val(240.0), Val(165.0), Val(272.0)),
+            Point::new(Val(290.0), Val(165.0), Val(114.0)),
+            Point::new(Val(290.0), Val(0.0), Val(114.0)),
+            Point::new(Val(240.0), Val(0.0), Val(272.0)),
+            Point::new(Val(130.0), Val(0.0), Val(65.0)),
+            Point::new(Val(82.0), Val(0.0), Val(225.0)),
+        ],
+        vec![
+            smallvec![0, 1, 2, 3],
+            smallvec![3, 0, 6, 4],
+            smallvec![3, 4, 5, 2],
+            smallvec![2, 5, 7, 1],
+            smallvec![1, 7, 6, 0],
+            smallvec![4, 6, 7, 5],
+        ],
+        Diffuse::new(Color::WHITE),
+    )?;
+
+    // Tall Block
+    scene.add_mesh(
+        smallvec![
+            Point::new(Val(423.0), Val(330.0), Val(247.0)),
+            Point::new(Val(265.0), Val(330.0), Val(296.0)),
+            Point::new(Val(314.0), Val(330.0), Val(456.0)),
+            Point::new(Val(472.0), Val(330.0), Val(406.0)),
+            Point::new(Val(423.0), Val(0.0), Val(247.0)),
+            Point::new(Val(472.0), Val(0.0), Val(406.0)),
+            Point::new(Val(314.0), Val(0.0), Val(456.0)),
+            Point::new(Val(265.0), Val(0.0), Val(296.0))
+        ],
+        vec![
+            smallvec![0, 1, 2, 3],
+            smallvec![4, 0, 3, 5],
+            smallvec![5, 3, 2, 6],
+            smallvec![6, 2, 1, 7],
+            smallvec![7, 1, 0, 4],
+            smallvec![4, 5, 6, 7],
+        ],
+        Diffuse::new(Color::WHITE),
+    )?;
+
+    let renderer = CoreRenderer::new(
+        camera,
+        scene,
+        Configuration {
+            ssaa_samples: 1024,
+            ..Configuration::default()
+        },
+    )?;
+    let image = renderer.render();
+    PngWriter::new(File::create("output/cornell-box.png")?).write(image)?;
+
+    Ok(())
+}
